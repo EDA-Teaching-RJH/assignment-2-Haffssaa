6706ab11ecb1c43adb24744aa403227fae4ec3b5
Haffssaa <hm573@kent.ac.uk>
2024-10-22 22:17:02 +0000

    z

--- AllTogether.py
+++ AllTogether.py
@@ -1,1 +1,17 @@
 ### Part Four -- your code goes here. 
+import random
+
+numbers = []
+
+for x in range(10):
+    numbers.append(random.randint(1, 100))
+print("Here is the original list:", numbers)
+
+x = 0
+while x < len(numbers):
+    if numbers[x] % 2 == 0:
+        numbers.pop(x)
+    else:
+        x += 1
+
+print("Remaining odd numbers:", numbers)
